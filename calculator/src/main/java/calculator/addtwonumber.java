34232f7627b593b3f3321d12552d4481446ae65b
Prachi Gadhikar <prachikawalkar5@gmail.com>
2026-01-28 05:40:36 +0530

    add four number

--- calculator/src/main/java/calculator/addtwonumber.java
+++ calculator/src/main/java/calculator/addtwonumber.java
@@ -4,9 +4,9 @@ public class addtwonumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10,b=20,c=30,d;
-		d=a+b+c;
-		System.out.println(d);
+		int a=10,b=20,c=30,d=9,e;
+		e=d+a+b+c;
+		System.out.println(e);
 
 	}
 
