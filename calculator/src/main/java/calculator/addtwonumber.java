d353a26d8a091a3d7f2310cf56c7e134f9588744
Prachi Gadhikar <prachikawalkar5@gmail.com>
2026-02-24 19:16:37 +0530

    added prachi here

--- calculator/src/main/java/calculator/addtwonumber.java
+++ calculator/src/main/java/calculator/addtwonumber.java
@@ -9,7 +9,7 @@ public class addtwonumber {
 		System.out.println(e);
 		c=b-c;
 		System.out.println(c);
-		System.out.println("I am shreelatha");
+		System.out.println("I am shreelatha and Prachi here");
 		c=a*b;
 		System.out.println(c);
 		
