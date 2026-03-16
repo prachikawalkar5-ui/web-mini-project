1a05f4f9d75128bcc562811f36cd5c061ee5a615
Prachi Gadhikar <prachikawalkar5@gmail.com>
2026-03-16 23:38:40 +0530

    added funtions

--- calculator/src/main/java/calculator/addtwonumber.java
+++ calculator/src/main/java/calculator/addtwonumber.java
@@ -2,19 +2,16 @@ package calculator;
 
 public class addtwonumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a=10,b=20,c=30,d=9,e;
-		e=d+a+b+c;
-		System.out.println(e);
-		c=b-c;
-		System.out.println(c);
-		c=a*b;
-		System.out.println(c);
-		System.out.println("This is CI/CD lab");
-		
-		
+	public int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
 
-	}
+    public int subtract(int b, int c) {
+        return b - c;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
 }
